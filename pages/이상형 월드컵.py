7a389db9b069b2153cfa52ef0a5e0487b998fe81
2530606-hash <2530606@sw.hs.kr>
2025-08-20 14:35:29 +0900

    Create 이상형 월드컵.py

---
+++ pages/이상형 월드컵.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('이상형 월드컵')
+
+#버튼 클릭 여부에 따라 실행
+if st.button("클릭하세요"):
+   st.write("버튼이 클릭되었습니다!")
+
+<p align="center">
+  <img src="images/pic1.png" width="300">
+  <img src="images/pic2.png" width="300">
+</p>
+
