7679ee436ae838ae8661afe835d495245cefe43f
2530606-hash <2530606@sw.hs.kr>
2025-08-21 09:53:03 +0900

    Update 이상형 월드컵.py

--- pages/이상형 월드컵.py
+++ pages/이상형 월드컵.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
-st.title('이상형 월드컵')
+st.title("이상형 월드컵")
 
-#버튼 클릭 여부에 따라 실행
+# 버튼 클릭 여부에 따라 실행
 if st.button("클릭하세요"):
-   st.write("버튼이 클릭되었습니다!")
+    st.write("버튼이 클릭되었습니다!")
 
-<p align="center">
-  <img src="pic1.png" width="300">
-  <img src="pic2.png" width="300">
-</p>
+# 두 개의 이미지를 가로로 나란히 배치
+col1, col2 = st.columns(2)
 
+with col1:
+    st.image("pic1.png", width=300)
+
+with col2:
+    st.image("pic2.png", width=300)
